1273e3dacbc823ae34be8fdae2e3021ac9214223
mecclesgoogle <markeccles@google.com>
2022-06-10 14:57:29 +1000

    use the onAuthStateChanged observer

--- src/Auth.js
+++ src/Auth.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './firestore';
 import {
-	getAuth, signInWithPopup, GoogleAuthProvider, signOut
+	getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged
 } from "firebase/auth"
 
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
+
 class Auth extends React.Component {
 
 	constructor(props) {
 		super(props);
-		// https://firebase.google.com/docs/reference/js/auth.user.md#user_interface
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				this.setState({
+					user: user
+				});
+			} else {
+				this.setState({
+					user: null
+				});
+			}
+		});
+
 		this.state = { user: auth.currentUser };
 		this.loginWithGoogle = this.loginWithGoogle.bind(this);
 		this.logoutWithGoogle = this.logoutWithGoogle.bind(this);
 	}
 
 	async loginWithGoogle(e) {
-		const result = await signInWithPopup(auth, provider);
-		console.log(result);
-		this.setState({
-			user: result.user
-		});
+		signInWithPopup(auth, provider);
+		console.log('Logged in');
 	}
 
 	async logoutWithGoogle(e) {
 		signOut(auth).then(() => {
 			console.log('Logged out');
-			this.setState({
-				user: null
-			});
 		}).catch((error) => {
 			console.error(error);
 		});
@@ -39,20 +45,17 @@ class Auth extends React.Component {
 
 	render() {
 		return <div className='auth'>
-			{this.state.user != null &&
-				<ul className="navbar-nav">
-					<li className='nav-item'>
-						<button className='btn btn-secondary' type='button' onClick={this.logoutWithGoogle}>Logout</button>
-					</li>
-					<li className='nav-item'>
-						<img width={50} src={this.state.user.providerData[0].photoURL} alt='profile'></img>
-					</li>
-				</ul>
-			}
-
 			{
-				this.state.user == null &&
-				<button className='btn btn-secondary' type='button' onClick={this.loginWithGoogle}>Login</button>
+				this.state.user == null ?
+					(<button className='btn btn-secondary' type='button' onClick={this.loginWithGoogle}>Login</button>) :
+					(<ul className="navbar-nav">
+						<li className='nav-item'>
+							<button className='btn btn-secondary' type='button' onClick={this.logoutWithGoogle}>Logout</button>
+						</li>
+						<li className='nav-item'>
+							<img width={50} src={this.state.user.providerData[0].photoURL} alt='profile'></img>
+						</li>
+					</ul>)
 			}
 		</div >;
 	}
